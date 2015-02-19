0d07cca14d601c618367ab3bb6836ef016bdeb1b
ssteuteville <ssteuteville@gmail.com>
2015-02-18 19:50:08 -0800

    attempt at getting login working

--- backend/rentlist/views.py
+++ backend/rentlist/views.py
@@ -426,7 +426,7 @@ def Login(request):
     user = authenticate(username=username, password=password)
     if user is not None:
         if user.is_active:
-            return JsonResponse(str(user.application_set().first().client_id), safe=False)
+            return JsonResponse(str(user.application_set.first().client_id), safe=False)
     #         # Redirect to a success page.
     #     else:
     #         # Return a 'disabled account' error message
