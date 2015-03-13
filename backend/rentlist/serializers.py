4f73169828d02c409bbe9bb40bc26e88fa574901
ssteuteville <ssteuteville@gmail.com>
2015-03-12 23:59:02 -0700

    Product Serializer fix

--- backend/rentlist/serializers.py
+++ backend/rentlist/serializers.py
@@ -113,7 +113,7 @@ class ProductSerializer(serializers.ModelSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
     owner = serializers.ReadOnlyField(source='owner.username')
     deposit = serializers.FloatField(default=0)
-    display_image = ImageSerializer() #serializers.PrimaryKeyRelatedField(queryset=Image.objects.all(), allow_null=True)s
+    display_image = serializers.PrimaryKeyRelatedField(queryset=Image.objects.all(), allow_null=True);
 
     class Meta:
         model = Product
