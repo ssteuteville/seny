91a2377dab804f8c32b482bf94362e277fd72cf6
ssteuteville <ssteuteville@gmail.com>
2015-04-02 23:02:29 -0700

    added dependencies

--- frontend/app/message/message.js
+++ frontend/app/message/message.js
@@ -6,7 +6,19 @@ angular.module('SENY.message', ['ngRoute', 'SenyData'])
         $routeProvider.when('/inbox', {
             templateUrl: 'message/inbox.html',
             controller: 'inboxController'
-        });
+        })
+            .when('/message/new', {
+                templateUrl: 'message/new.html',
+                controller: 'newMessageController'
+            })
+            .when('/message/new/:id', {
+                templateUrl: 'message/new.html',
+                controller: 'newMessageWithDestController'
+            })
+            .when('/message/reply/:id', {
+                templateUrl: 'message/reply.html',
+                controller: 'messageReplyController'
+            });
     }])
 
 .controller('inboxController', ['$scope', 'SenyData', '$location', function($scope, SenyData, $location){
@@ -34,11 +46,55 @@ angular.module('SENY.message', ['ngRoute', 'SenyData'])
         };
 
         $scope.reply = function(id){
-            $location.path('/message-reply/' + id);
-        }
+            $location.path('/message/reply/' + id);
+        };
+
+        $scope.new_msg = function(){
+            $location.path('/message/new');
+        };
         $scope.convertDate = function(date){
             var d = new Date(date);
 
             return d.toDateString() + " " + d.toLocaleTimeString();
         };
+    }])
+    .controller('newMessageController', ['$scope', 'SenyData', '$location', function($scope, SenyData, $location){
+            $scope.model = {};
+            $scope.destination = "";
+            $scope.send = function(){
+                SenyData.senyRequest('user-profiles/', 'GET', {owner: $scope.destination})
+                    .success(function(data){
+                        $scope.model.destination = data[0].owner_id;
+                        SenyData.senyRequest('messages/new/', 'POST', {}, $scope.model)
+                            .success(function(){
+                                $location.path('/inbox/');
+                            })
+                    })
+            }
+        }])
+    .controller('newMessageWithDestController', ['$scope', 'SenyData', '$location', '$routeParams',
+        function($scope, SenyData, $location, $routeParams){
+            $scope.model = {destination: $routeParams.id};
+            $scope.destination = $scope.model.destination;
+            $scope.send = function(){
+                SenyData.senyRequest('user-profiles/', 'GET', {owner: $scope.destination})
+                    .success(function(data){
+                        $scope.model.destination = data[0].owner_id;
+                        SenyData.senyRequest('messages/new/', 'POST', {}, $scope.model)
+                            .success(function(){
+                                $location.path('/inbox/');
+                            })
+                    })
+            }
+
+    }])
+    .controller('messageReplyController', ['$scope', 'SenyData', '$location', '$routeParams',
+        function($scope, SenyData, $location, $routeParams){
+            $scope.model = {thread: $routeParams.id}
+            $scope.send = function () {
+                SenyData.senyRequest('messages/', 'POST', {}, $scope.model)
+                    .success(function(data){
+                        $location.path('/inbox/');
+                    })
+            }
     }])
